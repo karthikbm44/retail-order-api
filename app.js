94dac0c925a5df9436d74f5d43a9f709729fd36b
Karthik B M <karthikbm0808@gmail.com>
2026-03-27 17:51:40 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 1010;
+
+app.get("/",(req,res)=>{
+    res.send("Retail Order API is running");
+})
+
+app.get("/orders",(req,res)=>{
+    res.json([
+        {
+            "id": 1,
+            "customerName": "John Doe",
+            status: "Processing"},
+            {
+                "id": 2,
+            "customerName": "John devil",
+            status: "delivered"
+            }
+    ])
+});
+
+app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
